d323bf2588a9af830c4db295f4bf996dfd84315d
Aadi Umrani <aadi.p.um@gmail.com>
2023-09-16 22:56:34 -0400

    Changed theme

--- src/App.js
+++ src/App.js
@@ -56,27 +56,31 @@ export default function Board() {
   } else if (squares.every((square) => square !== null)) {
     status = 'It\'s a draw!';
   } else {
-    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    status = 'Current player: ' + (xIsNext ? 'X' : 'O');
   }
 
   return (
     <div>
-      <div className="status">{status}</div>
-      <div className="board-row">
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
-      </div>
-      <div className="board-row">
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-      </div>
-      <div className="board-row">
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
-      </div>
+      <center>
+        <div className="status">{status}</div>
+        <div className='board'>
+          <div className="board-row">
+            {renderSquare(0)}
+            {renderSquare(1)}
+            {renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {renderSquare(3)}
+            {renderSquare(4)}
+            {renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {renderSquare(6)}
+            {renderSquare(7)}
+            {renderSquare(8)}
+          </div>
+        </div>
+      </center>
     </div>
   );
 }
